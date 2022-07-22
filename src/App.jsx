7d5cea29804187f3db41a92becf678d739ffa6f7
Sarthak Deore <90770726+sarthak-deore@users.noreply.github.com>
2022-07-22 22:52:30 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/navbar";
+import Home from "../components/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "../components/dashboard";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
